31500af6cd128a3847678e55c54645e9b7be9a7f
antonio <antonio@architux.com>
2017-07-02 20:00:04 +0200

    bobo

--- bobot-redir2.sh
+++ bobot-redir2.sh
@@ -4,15 +4,28 @@
 
 #server_target= export server_target=..
 
-declare -A my_map_array
+my_pipe_path="/tmp/jaimelespipes"
 
-while read line ; do 
-    my_source=$(echo "$line" | awk '{print$1}')
-    my_map_array[$my_source]=$(echo $line | awk '{print$2}')
-done < $2
+if [[ ! -p $my_pipe_path ]]; then
+    if ! mkfifo $my_pipe_path ; then
+        echo "Unable to create $my_pipe_path"
+        exit 3
+    fi  
+fi
+
+declare -A my_map_array
+declare -a my_index_map_array
+
 
 case $1 in
     all)
+    
+    while read line ; do 
+        my_source=$(echo "$line" | awk '{print$1}')
+        my_map_array[$my_source]=$(echo $line | awk '{print$2}')
+        my_index_map_array+=("$my_source")
+    done < $2
+
     for my_source in ${!my_map_array[@]} ; do
         sleep 0.1
         echo testing $server_target$my_source
@@ -30,31 +43,53 @@ case $1 in
     ;;
     partial)
 
+    sed -n "$3,$4"p $2 > $my_pipe_path &
+
+    while read line ; do 
+        my_source=$(echo "$line" | awk '{print$1}')
+        my_map_array[$my_source]=$(echo $line | awk '{print$2}')
+        my_index_map_array+=("$my_source")
+    done < $my_pipe_path
+
     echo starting at line $3 for ending at $4
     # testing seq
 
-    index_my_map_array=(${!my_map_array[@]})
-
-    for (( i=$3 ; i<=$4 ; i++ )) ; do
-        sleep 0.1
-	    #echo ${index_my_map_array[$i]}
-	    my_result=$(curl -m 3 -I -A Bobot-le-bot -s http://${server_target}${index_my_map_array[$i]} |& awk '/Location/ {print$2}'| tr -d '\r')
-        #echo $($my_result | tr -d '\r')
-        if [[ $my_result  !=  ${my_map_array[$my_source]} ]] ; then
-            #echo "line number ${i} - NOK - $my_resul"
-            echo "line number ${i} - NOK - ${my_result} and ${my_map_array[$my_source]} expected"
-        else
-            echo  "line ${i}: ok"
-        fi
-    done
+    lnb=$3
+    #echo "ra ${!my_index_map_array[@]}"
+    for i in ${!my_index_map_array[@]} ; do
+             (( lnb++ )) 
+            echo "lnb: $lnb"
+            sleep 0.1
+	        #my_result=$(curl -m 3 -I -A Bobot-le-bot -s http://${server_target}${my_index_map_array[$i]} |& awk '/Location/ {print$2}'| tr -d '\r')
+	        my_result=$(curl -m 3 -I -A Bobot-le-bot -s http://${server_target}${my_index_map_array[$lnb]} |& awk '/Location/ {print$2}'| tr -d '\r')
+            echo "index: $i"
+            echo "source: ${my_index_map_array[$i]}"
+            echo "result: $my_result"
+            #if [[ $my_result  !=  ${my_map_array[${my_index_map_array[$i]}]} ]] ; then
+            #    echo "line number $lnb - NOK - obtain ${my_result} for ${my_map_array[$my_source]} expected"
+            #else
+            #    echo  "line $lnb: ok"
+            #fi
+        done
     ;;
     random)
 
     declare -a my_random_array 
 
     for i in {0..199} ; do
-        my_random_array+="$i"
-        index_my_map_array=(${!my_map_array[@]})
+        my_random_array+="$RANDOM "
+    done
+
+    for i in ${my_random_array[@]} ; do
+        sleep 0.1
+        echo "number is $i"
+	    my_result=$(curl -m 3 -I -A Bobot-le-bot -s http://${server_target}${my_index_map_array[$i]} |& awk '/Location/ {print$2}'| tr -d '\r')
+        #echo $my_result
+        if [[ $my_result  !=  ${my_map_array[${my_index_map_array[$i]}]} ]] ; then
+            echo "line number ${i} - NOK - obtain ${my_result} for ${my_map_array[$my_source]} expected"
+        else
+            echo  "line ${i}: ok"
+        fi
     done
     
 esac    
